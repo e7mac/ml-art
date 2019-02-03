79552dba2011229fad3ec404df320aec052fdaaf
Mayank Sanganeria <mayank.sanganeria@leanplum.com>
2019-02-02 18:47:30 -0800

    fixes;

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
-from audio_tools import *
-from video_tools import *
+# from audio_tools import *
+# from video_tools import *
+import os
+from requests import get  # to make GET request
 
-timeSlice = 1 #seconds
+def createDirectory(dirName):
+    if not os.path.exists(dirName):
+        os.makedirs(dirName)
+
+def makeMainDirectories():
+    createDirectory('input')
+    createDirectory('input/raw')
+    createDirectory('input/preprocessed')
+    createDirectory('output')
+
+def download(url, dirName):
+    file_name = dirName + '/' + url.split('/')[-1]
+    with open(file_name, "wb") as file:
+        response = get(url)
+        file.write(response.content)
+
+makeMainDirectories()
 
 sample = "https://s3-us-west-2.amazonaws.com/e7mac.com/BreakMeMadeira-small.mov"
 # sample = "https://s3-us-west-2.amazonaws.com/e7mac.com/BreakMeMadeira-medium.mov"
 # sample = "https://s3-us-west-2.amazonaws.com/e7mac.com/BreakMeMadeira-full.mov"
+download(sample, 'input/raw/')
 
-audioSlice("BreakMeMadeira31.mov", timeSlice)
+timeSlice = 1 #seconds
+
+# audioSlice("BreakMeMadeira31.mov", timeSlice)
 # videoSlice("BreakMeMadeira31.mov", timeSlice, 10, 0.1)
--- video_tools.py
+++ video_tools.py
@@ -1,6 +1,6 @@
-def videoSlice(filename, sliceDuration, fps, sizeFraction):
-    import cv2
-    print(cv2.__version__)
+import cv2
+
+def videoSlice(filename, sliceDuration, fps, sizeFraction):    
     vidcap = cv2.VideoCapture(filename)
     success,image = vidcap.read()
     image = cv2.resize(image, (0,0), fx=sizeFraction, fy=sizeFraction)
@@ -11,7 +11,7 @@ def videoSlice(filename, sliceDuration, fps, sizeFraction):
     desired_fps = fps
     while success:
         vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*1000.0 / desired_fps))
-        outputFilename = "output_video/slice_%d_frame%d.jpg" % (sliceNum, count)
+        outputFilename = "output/slice_%d_frame%d.jpg" % (sliceNum, count)
         cv2.imwrite(outputFilename, image)     # save frame as JPEG file
         success,image = vidcap.read()
         if success == True:
