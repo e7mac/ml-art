f35ebd44d220aab0db47b085b7b90d9902b95388
Mayank Sanganeria <mayank.sanganeria@leanplum.com>
2019-02-20 16:38:38 -0800

    single image training

--- synesthesia/main.py
+++ synesthesia/main.py
@@ -121,7 +121,8 @@ def main():
     train_op = tf.contrib.training.create_train_op(loss_op, optimizer)
 
     # Batch 
-    for batch in range(int((av.video.frameCount - 1 - _NUM_TEMPORAL_FRAMES) / batchSize)):
+    # for batch in range(int((av.video.frameCount - 1 - _NUM_TEMPORAL_FRAMES) / batchSize)):
+    for batch in range(1):
         images_arr, audios_arr = getTemporalFramesBatchFromData(av, batch, batchSize)
         feed_dict = {
             inputs: images_arr,
@@ -134,7 +135,8 @@ def main():
     print('training done')
     audio_pred_array = []
     # Infer 
-    for batch in range(int((av.video.frameCount - 1 - _NUM_TEMPORAL_FRAMES) / batchSize)):
+    # for batch in range(int((av.video.frameCount - 1 - _NUM_TEMPORAL_FRAMES) / batchSize)):
+    for batch in range(1):
         images_arr, audios_arr = getTemporalFramesBatchFromData(av, batch, batchSize)
         feed_dict = {
             inputs: images_arr,
