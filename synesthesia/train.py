13fb01fae6aa23f4ecd767e4318d72f392bb1ddc
Mayank Sanganeria <mayank.sanganeria@leanplum.com>
2019-02-26 17:49:13 +0100

    lower lr

--- synesthesia/train.py
+++ synesthesia/train.py
@@ -13,7 +13,7 @@ import tensorflow as tf
 _NUM_TEMPORAL_FRAMES = 5
 _AUDIO_DIMS = 1./30 * 44100 #per frame for 30 fps video
 _IMAGE_CROP_SIZE = 200
-_LEARNING_RATE = 1e-7
+_LEARNING_RATE = 1e-8
 
 
 def _infer(output, feed_dict):
