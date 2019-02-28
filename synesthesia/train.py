8ab74651cae9ba2e39dde09c7cfb9617f2d0f47b
Mayank Sanganeria <mayank.sanganeria@leanplum.com>
2019-02-28 09:15:40 +0100

    radically reduced lr

--- synesthesia/train.py
+++ synesthesia/train.py
@@ -13,7 +13,7 @@ import tensorflow as tf
 _NUM_TEMPORAL_FRAMES = 5
 _AUDIO_DIMS = 1./30 * 44100 #per frame for 30 fps video
 _IMAGE_CROP_SIZE = 200
-_LEARNING_RATE = 1e-10
+_LEARNING_RATE = 1e-15
 
 
 def _infer(output, feed_dict):
